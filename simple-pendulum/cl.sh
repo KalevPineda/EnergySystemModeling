60b23b0d240a33c7512f2bbe6d99bfbed35453ee
g0nn3r <kalev.pineda.dev@gmail.com>
2025-06-04 01:53:29 -0600

    Simple pendulum init

---
+++ simple-pendulum/cl.sh
@@ -0,0 +1,8 @@
+echo "Iniciando limpieza ..."
+rm -r *.pdf
+echo "   [OK] Archivos .pdf limpiados"
+rm -r *.png
+echo "   [OK] Archivos .png limpiados"
+rm -r *.svg
+echo "   [OK] Archivos .svg limpiados"
+
